ff0f1876a7a20746e85b8396a920372c748d9e11
Eric Sun <eric.sun.ecs92@yale.edu>
2023-06-28 16:49:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Salary Prediction ML App
+This is a web application to predict salary of Developers around the world based on the 2022 Stack Overflow Annual Developer Survey.
+
+# Made By: Eric Sun
+
+# Logs: 
+1. Download data from (https://insights.stackoverflow.com/survey)
+2. Clean Data
+3. Test different training models (RandomForestRegression had best results)
+4. Build web application using streamlit
+5. Deploy
---
+++ predict_page.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', "rb") as f:
+        data = pickle.load(f)
+    return data
+
+data = load_model()
+
+decision_forest_model = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+    
+    st.write("""### We need some info to predict the salary""")
+    
+    countries = (
+        "United States",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",                
+        "Brazil",              
+        "France",                 
+        "Spain",                  
+        "Australia",              
+        "Netherlands",            
+        "Poland",                 
+        "Italy",                 
+        "Russian Federation",     
+        "Sweden"                 
+    )
+    
+    educations = (
+        "Less than a Bachelor’s",
+        "Post Grad",
+        'Master’s degree',
+        'Bachelor’s degree'
+    )
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education", educations)
+    
+    experience = st.slider("Years of Experience", 0, 50, 3)
+    
+    ok = st.button("Calculate Salary")
+    if ok:
+        user_input = np.array([[country, education, experience]])
+        user_input[:, 0] = le_country.transform(user_input[:, 0])
+        user_input[:, 1] = le_education.transform(user_input[:, 1])
+        user_input = user_input.astype(float)
+        
+        salary = decision_forest_model.predict(user_input)
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+    
+    
+        
+    
